2269a8d58f7e49372b823d9171948b3ae1993539
nomis51 <nomis51@users.noreply.github.com>
2022-09-15 18:38:33 -0300

    Update demo to show navigation starting and navigation completed usage

--- cmd/demo/main.go
+++ cmd/demo/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/jchv/go-webview2"
@@ -23,6 +24,26 @@ func main() {
 	}
 	defer w.Destroy()
 	w.SetSize(800, 600, webview2.HintFixed)
+	_ = w.NavigationStarting(onNavigationStarting)
+	_ = w.NavigationCompleted(onNavigationCompleted)
 	w.Navigate("https://en.m.wikipedia.org/wiki/Main_Page")
 	w.Run()
+}
+
+func onNavigationCompleted(httpStatusCode int32, isSuccess bool, navigationId uint64, webErrorStatus int32) {
+	fmt.Println("navigation completed:")
+	fmt.Println("http status code: ", httpStatusCode)
+	fmt.Println("success: ", isSuccess)
+	fmt.Println("navigation id: ", navigationId)
+	fmt.Println("web error status: ", webErrorStatus)
+}
+
+func onNavigationStarting(additionalAllowedFrameAncestors string, isRedirected bool, isUserInitiated bool, navigationId uint64, uri string) bool {
+	fmt.Println("navigation starting:")
+	fmt.Println("redirected: ", isRedirected)
+	fmt.Println("user initiated: ", isUserInitiated)
+	fmt.Println("navigation id: ", navigationId)
+	fmt.Println("additional allowed frame ancestors: ", additionalAllowedFrameAncestors)
+	fmt.Println("uri: ", uri)
+	return false
 }
